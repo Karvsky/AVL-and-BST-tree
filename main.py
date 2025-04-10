1d553ca5fe88da95fcdccbcac42a3d1d73b3c4a9
Karvsky <karvsky@gmail.com>
2025-04-10 20:10:14 +0200

    3

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-
 from AVL_tree import *
+from pomiary_czasu import create_tree_with_bisect_method, find_max_element, inorder_traversal
 
 
 print("Wybierz opcje: \n 1 AVL tree\n 2 BST tree\n 3 wyjscie")
 choice = input()
 if choice == "1":
     print(menu())
+    print(create_tree_with_bisect_method())
+    print(find_max_element())
+    print(inorder_traversal())
 #elif choice == '2':
 
 elif choice == '3':
--- pomiary_czasu.py
+++ pomiary_czasu.py
@@ -1,35 +1,36 @@
 import time
-import random
 from AVL_tree import AVLTree
 
-# Funkcja do generowania posortowanych ciągów liczb naturalnych
-def generate_sorted_sequences(n):
-    return sorted(random.sample(range(1, 10 * n), n))
-
-# Funkcja do mierzenia czasu operacji na drzewie
-def measure_avl_operations():
-    for n in [10, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]:
-        sequence = generate_sorted_sequences(n)
+def create_tree_with_bisect_method():
+    n_values = [5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000]
+    for n in n_values:
+        sorted_sequence = list(range(1, n + 1))
         tree = AVLTree()
 
-        # Mierzymy czas wstawiania elementów
         start_time = time.time()
-        for num in sequence:
-            tree.add(num)
-        end_time = time.time()
-        print(f"Czas wstawiania {n} elementów: {end_time - start_time:.6f} sekund")
 
-        # Mierzymy czas wyszukiwania elementu o maksymalnej wartości
-        start_time = time.time()
-        max_value, path = tree.find_max()
-        end_time = time.time()
-        print(f"Czas wyszukiwania maksymalnej wartości ({max_value}): {end_time - start_time:.6f} sekund")
+        for el in sorted_sequence:
+            tree.add(el)
 
-        # Mierzymy czas wypisania wszystkich elementów drzewa w porządku in-order
-        start_time = time.time()
-        tree.inorder(tree.root)
         end_time = time.time()
-        print(f"Czas wypisania {n} elementów w porządku in-order: {end_time - start_time:.6f} sekund")
 
-if __name__ == "__main__":
-    measure_avl_operations()
+        print(f"Czas tworzenia drzewa z {n} elementami: {end_time - start_time:.6f} sekund")
+
+def find_max_element():
+    tree = AVLTree()
+    for el in range(1, 100000):
+        tree.add(el)
+    start_time = time.time()
+    max_value, _ = tree.find_max()
+    end_time = time.time()
+    print(f"Największy element: {max_value}, Czas wyszukiwania: {end_time - start_time:.10f} sekund")
+
+def inorder_traversal():
+    tree = AVLTree()
+    for el in range(1, 5000):
+        tree.add(el)
+    start_time = time.time()
+    tree.inorder(tree.root)
+    end_time = time.time()
+    print('\n')
+    print(f"Czas wykonania operacji in-order: {end_time - start_time:.10f} sekund")
